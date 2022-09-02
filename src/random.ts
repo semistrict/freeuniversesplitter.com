f3b3f64fea308b20487210f634445582803efa83
Ramon Nogueira <ramon@echophase.com>
2022-09-02 09:52:14 -0400

    Remove fake localhost

--- src/random.ts
+++ src/random.ts
@@ -7,23 +7,17 @@ let nextNumber = 1
 
 export async function getRandom(): Promise<RandomResult> {
     let randomNum
-    let fake = true
 
-    if (location.hostname == "localhost") {
-        randomNum = nextNumber++
-    } else {
-        let resp = await fetch("https://api.freeuniversesplitter.com/split", {
-            method: 'POST',
-            cache: 'no-cache',
-        })
-        let body = await resp.json()
-        if (!body.success) {
-            window.alert(`Oh no! Something went wrong... (${resp.status})`)
-            throw `request failed! ${resp.status}`
-        }
-        randomNum = body.data[0]
-        fake = false
+    let resp = await fetch("https://api.freeuniversesplitter.com/split", {
+        method: 'POST',
+        cache: 'no-cache',
+    })
+    let body = await resp.json()
+    if (!body.success) {
+        window.alert(`Oh no! Something went wrong... (${resp.status})`)
+        throw `request failed! ${resp.status}`
     }
+    randomNum = body.data[0]
 
-    return {randomNum, fake}
+    return {randomNum, fake: false}
 }
